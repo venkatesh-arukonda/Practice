af88e43d30e0ebce973964ded4568d412e3a142b
Venkatesh Arukonda <venkateshgoud.venky43@gmail.com>
2024-08-18 12:58:57 +0530

    first commit

---
+++ Test.js
@@ -0,0 +1,14 @@
+alert("Hello");
+alert(1+2+3);
+[1,2].forEach(alert);
+alert("some code");
+"use stict";
+prompt("Hello",'150');
+let Hello = "Hello World";
+let message;
+message = Hello;
+document.write(message);
+let str1 = "Hello";
+let str2 = "To";
+let welcome = `${str1} ${str2} Eveyone`;
+alert(welcome);
